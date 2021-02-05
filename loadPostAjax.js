b6b707b86c6bf749ee5e01f268f4ff6d3cae1578
Nikita Kukshynsky <66539239+enkeipy@users.noreply.github.com>
2021-02-05 12:04:21 -0600

    Update loadPostAjax.js

--- loadPostAjax.js
+++ loadPostAjax.js
@@ -1,27 +1,27 @@
 jQuery(document).ready(function($) {
 
-    var window_height = $( window ).height();
+    var windowHeight = $( window ).height();
 
-    var process_ajax = true;
+    var processAjax = true;
 
     $(window).scroll(function() {
         var scroll = $(window).scrollTop();
-        var last_article = $( "article" ).last();
-        var last_article_height = last_article.height();
-        var last_article_offset_top = last_article.offset().top;
+        var lastArticle = $( "article" ).last();
+        var lastArticleHeight = lastArticle.height();
+        var lastArticleOffsetTop = lastArticle.offset().top;
 
-        var last_article_to_bottom = window_height + scroll - last_article_offset_top - last_article_height;
+        var lastArticleToBottom = windowHeight + scroll - lastArticleOffsetTop - lastArticleHeight;
 
-        if (last_article_to_bottom > 0 && process_ajax == true) {
-            process_ajax = false;
-            var id = last_article.attr("id").substring(5);
+        if (lastArticleToBottom > 0 && processAjax == true) {
+            processAjax = false;
+            var id = lastArticle.attr("id").substring(5);
             this2 = this;
-            $.post(my_ajax_obj.ajax_url, {
+            $.post(load_post_obj.ajax_url, {
                 action: "load_previous_post",
                 id: id
             }, function(data) {
-                last_article.after(data);
-                process_ajax = true;
+                lastArticle.after(data);
+                processAjax = true;
             });
         }
 
