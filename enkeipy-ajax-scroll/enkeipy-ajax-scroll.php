e64aae237d6e33ca0807cc5522fb7e36754837c3
Nikita Kukshinski <nkstatedev@gmail.com>
2021-02-06 16:47:19 -0600

    WordPress plugin init structure

---
+++ enkeipy-ajax-scroll/enkeipy-ajax-scroll.php
@@ -0,0 +1,12 @@
+/**
+ * Plugin Name:       Infinite Ajax Post Scroll
+ * Plugin URI:        https://github.com/enkeipy/Infinite-Ajax-Post-Scroll
+ * Description:       Basic Infinite Ajax Post Scroll for WordPress.
+ * Version:           1.0.0
+ * Requires at least: 5.6
+ * Requires PHP:      7.2
+ * Author:            Nikita Kukshynsky
+ * Author URI:        https://enkeipy.dev/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ */
